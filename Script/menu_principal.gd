133d636d43530660c59c8f6780b09a764396aa08
LucianoOrd <70179197+LucianoOrd@users.noreply.github.com>
2025-01-23 16:31:18 -0300

    agregado logica de menu
    Se agrego la logica a los botones del menu. La logica del boton de jugar esta comentada

---
+++ Script/menu_principal.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+func _on_start_button_pressed() -> void:
+	pass
+	#get_tree().change_scene_to_file("res://nombre_escena.tscn")
+
+func _on_settings_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
