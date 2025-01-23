43bf2cb1ceb733cefdc906cbe3b125b8c1ad96b3
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2025-01-23 11:55:06 -0300

    Agregado sistema de base de datos para evidencias

---
+++ Scripts/Resources/Evidence.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name Evidence
+
+@export var title:String
+@export var img:String
+@export var description: String
---
+++ Scripts/Resources/EvidenceDB.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name EvidenceDB
+
+@export var evidenceDB: Array[Evidence]=[]
