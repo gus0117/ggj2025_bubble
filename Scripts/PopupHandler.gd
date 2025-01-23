f1719a89a28367125e6cf945052e2f17631627bb
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2025-01-23 00:51:56 -0300

    Agregado efecto fade in para el blur de la imagen y el texto.

---
+++ Scripts/PopupHandler.gd
@@ -0,0 +1,16 @@
+extends Control
+class_name PopupHandler
+
+@onready var blurPanel: Panel = $Panel/Body/Blur
+@onready var descriptionPanel: MarginContainer = $Panel/Body/Blur/Description
+@onready var animation_player: AnimationPlayer = $Panel/AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	FadeInAnimation()
+
+func OpenPopup() -> void:
+	pass
+
+func FadeInAnimation() -> void:
+	animation_player.play("FadeIn")
