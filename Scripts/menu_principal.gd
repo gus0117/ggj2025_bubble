251c1843a28d4bd985f5b31c413d009606b8d9eb
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2025-01-26 15:13:12 -0300

    Agregada logica para desaparecer el boton de salida en OS web

--- Scripts/menu_principal.gd
+++ Scripts/menu_principal.gd
@@ -1,7 +1,11 @@
 extends Control
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var exit_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button
 
 func _ready() -> void:
+	if OS.get_name() == "Web":
+		exit_button.visible = false
+	
 	animation_player.play("MostrarTitulo")
 
 func _on_start_button_pressed() -> void:
