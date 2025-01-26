fde7c0d8049c8f44924073fa3088bbe24aa97d0f
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2025-01-26 15:42:44 -0300

    Actualizada logica del feedback para los sprites

--- Scripts/feedback.gd
+++ Scripts/feedback.gd
@@ -1,20 +1,20 @@
 extends Control
 class_name Feedback
 
-@export var angry_emoji: String
-@export var calm_emoji: String
-@onready var content: Label = $content
+@export var angry_emoji: Texture2D
+@export var calm_emoji: Texture2D
+@onready var content: TextureRect = $content
 
 func ShowAngry() -> void:
-	content.text = angry_emoji
+	content.texture = angry_emoji
 	Animate()
 
 func ShowCalm() -> void:
-	content.text = calm_emoji
+	content.texture = calm_emoji
 	Animate()
 
 func Animate() -> void:
-	content.position = Vector2(-19, 0)
+	content.position = Vector2(-14, 0)
 	content.modulate = Color(1,1,1,1)
 	var tween := get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
 	tween.tween_property(content, "position", content.position - Vector2(0, 50), 0.8)
