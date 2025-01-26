6a2b4ac18f8ad7983f6b3e040d37c172c56a3e94
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2025-01-25 23:14:40 -0300

    Actualizado contenido del menu principal y animacion

--- Scripts/menu_principal.gd
+++ Scripts/menu_principal.gd
@@ -1,5 +1,8 @@
 extends Control
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+func _ready() -> void:
+	animation_player.play("MostrarTitulo")
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_game.tscn")
